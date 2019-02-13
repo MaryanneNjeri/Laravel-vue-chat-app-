ddef25a1d00a1b6c7e98a63b5796a0af03cc4bc0
MaryAnne45 <mary.jereh@gmail.com>
2019-02-13 15:13:50 +0300

    added a funcition to tell a user whether or not the message is for a signed in user

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    //belongs to 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    // this is done to prevent the  mass assignment exception
+    protected $fillable = ['body'];
+    // this tells us whether the message is our own or the  user's we chat in 
+    protected $appends = ['selfMessage'];
+
+    // we add a function that tells us whether the message is owned by a signed user or another user.
+    public function getSelfMessageAttribute()
+    {
+        return $this->user_id === auth()->user()->id();
+    }
 }
