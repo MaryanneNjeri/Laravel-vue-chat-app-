cbaa6f4e31c889c2e9a31f33a8c73c5f03e634cc
MaryAnne45 <mary.jereh@gmail.com>
2019-02-13 15:06:25 +0300

    added message table and created a one to many relationship with the user model one user has many messages

--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //create a one to many relationship one user can have many messages
+    public function messages()
+    { 
+        return $this->hasMany(Message::class);
+
+    }
 }
--- database/migrations/2019_02_13_113901_create_messages_table.php
+++ database/migrations/2019_02_13_113901_create_messages_table.php
@@ -15,7 +15,10 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->text('body');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
