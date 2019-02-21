e2f44db65644a5cc859ca6c86c37aee7469877d5
MaryAnne45 <mary.jereh@gmail.com>
2019-02-21 17:24:49 +0300

    we query the messages of a specific user

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    // 
+    public function index()
+    { // we get the messages for specific user remember it has a one to many relationship 
+        
+        $messages = Message::with([user])->get();
+        return reponse()->json($messages);
+    }
 }
